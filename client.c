df3d7eccdb1e17ee32f16f85b9d5cc1448754c1e
DadiaZ24 <diogo-uds@hotmail.com>
2024-04-28 15:08:54 +0100

    Minitalk

---
+++ client.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 13:34:01 by ddias-fe          #+#    #+#             */
+/*   Updated: 2024/04/28 13:34:01 by ddias-fe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+void send_to_server(int pid, char str)
+{
+    int bit;
+
+    bit = 0;
+    while (bit < 8)
+    {
+        if (((str >> bit) & 1) == 1)
+            kill (pid, SIGUSR1);
+        else
+            kill (pid, SIGUSR2);
+        usleep(100);
+        bit++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int     pid;
+    int     i;
+    char    *msg;
+
+    if (argc != 3)
+        return (ft_printf("ERROR: Wrong usage of the program. Use './client [PID] [Message]\n"));
+    i = -1;
+    msg = "\033[1;32mCLIENT MESSAGE: \033[0m";
+    pid = ft_atoi(argv[1]);
+    while (msg[++i])
+        send_to_server(pid, msg[i]);
+    i = -1;
+    while (argv[2][++i])
+        send_to_server(pid, argv[2][i]);
+    send_to_server(pid, '\n');
+    return (0);
+}
---
+++ server.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 13:17:39 by ddias-fe          #+#    #+#             */
+/*   Updated: 2024/04/28 13:17:39 by ddias-fe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+void    welcome_message(void)
+{
+    ft_printf("\033[1;32m_______________________________________________________________________\n\n");
+    ft_printf("\033[1;31m __   __  ___   __    _  ___   _______  _______  ___      ___   _ \n");
+    ft_printf("\033[1;31m|  |_|  ||   | |  |  | ||   | |       ||   _   ||   |    |   | | |\n");
+    ft_printf("\033[1;31m|       ||   | |   |_| ||   | |_     _||  |_|  ||   |    |   |_| |\n");
+    ft_printf("\033[1;31m|       ||   | |       ||   |   |   |  |       ||   |    |      _|\n");
+    ft_printf("\033[1;31m|       ||   | |  _    ||   |   |   |  |       ||   |___ |     |_ \n");
+    ft_printf("\033[1;32m| ||_|| ||   | | | |   ||   |   |   |  |   _   ||       ||    _  |\n");
+    ft_printf("\033[1;32m|_|   |_||___| |_|  |__||___|   |___|  |__| |__||_______||___| |_|\n\n\033[0m");
+    ft_printf("\033[1;31mAuthor: \033[32mddias-fe                                  \033[1;31mServer PID: \033[32m%d\n", getpid());
+    ft_printf("_______________________________________________________________________\n\n");
+    ft_printf("                                 SERVER\n\n\033[0m");
+}
+
+void    receive_message(int bitsignal)
+{
+    static int     bit;
+    static unsigned char    c;
+
+    if (bitsignal == SIGUSR1)
+        c |= (1 << bit);
+    bit += 1;
+    if (bit == 8)
+    {
+        ft_printf("%c", c);
+        bit = 0;
+        c = 0;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    if (argc != 1)
+        return (ft_printf("\033[0;31mERROR: You should only use './server' and nothing more!\n"));
+    welcome_message();
+    ft_printf("Waiting for client interaction...\n\033[1;32m");
+    while (1)
+    {
+        signal(SIGUSR1, receive_message);
+        signal(SIGUSR2, receive_message);
+    }
+    return (0);
+}
